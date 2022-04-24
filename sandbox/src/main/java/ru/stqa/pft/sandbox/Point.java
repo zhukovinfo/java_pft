df8367dfbef0f1574c0acccaf4505ab1c55037e5
Andrey Zhukov <zhukovinfo@gmail.com>
2022-04-24 15:54:02 +0400

    Reformatted Task2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,12 +5,12 @@ public class Point {
   public int x;
   public int y;
 
-  public Point(int x, int y){
+  public Point(int x, int y) {
     this.x = x;
     this.y = y;
   }
 
-  public double distance(Point p2){
-    return Math.sqrt(Math.pow((p2.x - this.x),2) + Math.pow((p2.y - this.y),2));
+  public double distance(Point p2) {
+    return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
   }
 }
