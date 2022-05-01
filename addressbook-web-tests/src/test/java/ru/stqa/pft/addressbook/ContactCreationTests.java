8f703c45efd05355604c639acc5c1313f82a5d49
Andrey Zhukov <zhukovinfo@gmail.com>
2022-05-02 00:21:22 +0400

    fixed test annotations

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -7,15 +7,15 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ContactCreationTests {
 
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -125,7 +125,7 @@ public class ContactCreationTests {
     wd.findElement(By.linkText("Logout")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() {
     logout();
     wd.quit();
