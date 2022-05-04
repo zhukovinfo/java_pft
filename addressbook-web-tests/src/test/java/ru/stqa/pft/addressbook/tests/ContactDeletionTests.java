0258341f2f6b69ffa86d271a41a27e8cc94dcbd3
Andrey Zhukov <zhukovinfo@gmail.com>
2022-05-04 14:18:29 +0400

    added task 5

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class ContactDeletionTests extends TestBase {
+
+  @Test
+  public void testContactDeletion(){
+
+  }
+
+}
